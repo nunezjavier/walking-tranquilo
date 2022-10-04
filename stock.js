42a28706db872113aaf59d82b93d0f3a0bef003c
nunezjavier <javin7298@gmail.com>
2022-10-04 18:46:32 -0300

    primera subida prueba

---
+++ stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "jordan 1", cantidad: 1, precio: 1200, talle: "L", img: './img/jordan1.jpg'},
+    {id: 2, nombre: "jordan 2", cantidad: 1, precio: 1100, talle: "L", img: './img/jordan2.jpg'},
+    {id: 3, nombre: "jordan 3", cantidad: 1, precio: 1200, talle: "M", img: './img/jordan3.jpg'},
+    {id: 4, nombre: "jordan 4", cantidad: 1, precio: 1400, talle: "M", img: './img/jordan4.jpg'},
+    {id: 5, nombre: "jordan 5", cantidad: 1, precio: 1200, talle: "S", img: './img/jordan5.jpg'},
+    {id: 6, nombre: "jordan 6", cantidad: 1, precio: 1500, talle: "S", img: './img/jordan6.jpg'},
+    {id: 7, nombre: "jordan 7", cantidad: 1, precio: 500, talle: "L", img: './img/jordan7.jpg'},
+    {id: 8, nombre: "jordan 8", cantidad: 1, precio: 500, talle: "L", img: './img/jordan8.jpg'},
+]
